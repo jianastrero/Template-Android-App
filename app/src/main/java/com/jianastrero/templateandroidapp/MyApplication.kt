e9f515b31d0450c284e719755e5816e8580a59cb
Jian James P. Astrero <jianjamesastrero@gmail.com>
2023-07-26 05:09:09 +0800

    Add Custom Application class and update AndroidManifest.xml
    Added a custom Application class 'MyApplication.kt' to set global application state. Also, updated AndroidManifest.xml to declare 'MyApplication' as the application class. These changes provide us with a place to maintain global application state and execute code that should run before any Android components, services, or activities are initialized.

---
+++ app/src/main/java/com/jianastrero/templateandroidapp/MyApplication.kt
@@ -0,0 +1,9 @@
+package com.jianastrero.templateandroidapp
+
+import android.app.Application
+
+class MyApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
