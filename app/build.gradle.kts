d9ab73c854ee1032437faee94917bb449a500587
Jian James P. Astrero <jianjamesastrero@gmail.com>
2023-07-26 05:04:57 +0800

    Update AndroidX and Compose dependencies in build.gradle.kts
    The dependencies are updated to use the latest versions of AndroidX and Android Compose for better performance and stability of the app. The build.gradle.kts is also restructured for better readability, separating main, Compose and unit test dependencies with comments.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,20 +47,28 @@ android {
 }
 
 dependencies {
-
-    implementation("androidx.core:core-ktx:1.9.0")
+    // Main AndroidX Dependencies
+    implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
     implementation("androidx.activity:activity-compose:1.7.2")
+
+    // Compose Dependencies
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+
+    // Unit Test Dependencies
     testImplementation("junit:junit:4.13.2")
+
+    // Android Test Dependencies
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+
+    // Debug Dependencies
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 }
