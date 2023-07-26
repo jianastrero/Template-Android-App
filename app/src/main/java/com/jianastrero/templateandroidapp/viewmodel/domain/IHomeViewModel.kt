baea2cf8cc219847b508cc6aeaffe09968fd4f5f
Jian James P. Astrero <jianjamesastrero@gmail.com>
2023-07-26 13:09:25 +0800

    Add IHomeViewModel interface for Home state management
    Added a new Interface 'IHomeViewModel' under domain package of the Android application. This interface includes a 'state' val of 'HomeState' type and an 'updateState' function. It is designed to handle state changes and updates of 'HomeState' for the Home page. This will provide a better structure for state manipulation and managing data flow from UI. It also includes a static 'Preview' object for testing and previewing purposes.

---
+++ app/src/main/java/com/jianastrero/templateandroidapp/viewmodel/domain/IHomeViewModel.kt
@@ -0,0 +1,21 @@
+package com.jianastrero.templateandroidapp.viewmodel.domain
+
+import com.jianastrero.templateandroidapp.state.home.HomeState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+
+
+interface IHomeViewModel {
+    val state: StateFlow<HomeState>
+
+    fun updateState(state: HomeState)
+
+    companion object {
+        val Preview = object : IHomeViewModel {
+            override val state: StateFlow<HomeState>
+                get() = MutableStateFlow(HomeState())
+
+            override fun updateState(state: HomeState) {}
+        }
+    }
+}
