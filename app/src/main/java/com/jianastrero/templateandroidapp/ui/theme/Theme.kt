afb57f6fda041650c3c57ed774f95534328b964b
Jian James P. Astrero <jianjamesastrero@gmail.com>
2023-07-26 10:15:32 +0800

    Update Theme.kt to include a Surface component
    Added Surface component in Theme.kt to fill the entire screen area with color and content. This was done to handle UI scenarios where individually composited components do not cover the entire screen, leading to uncolored patches. The Surface component now wraps MaterialTheme, ensuring that the background color is uniformly applied, images are drawn correctly, and content is correctly layered across the entire screen. Additional classes imported to support this feature.

--- app/src/main/java/com/jianastrero/templateandroidapp/ui/theme/Theme.kt
+++ app/src/main/java/com/jianastrero/templateandroidapp/ui/theme/Theme.kt
@@ -3,13 +3,16 @@ package com.jianastrero.templateandroidapp.ui.theme
 import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
@@ -80,7 +83,12 @@ fun TemplateAndroidAppTheme(
 
     MaterialTheme(
         colorScheme = colorScheme,
-        typography = Typography,
-        content = content
-    )
+        typography = Typography
+    ) {
+        Surface(
+            content = content,
+            color = MaterialTheme.colorScheme.background,
+            modifier = Modifier.fillMaxSize()
+        )
+    }
 }
