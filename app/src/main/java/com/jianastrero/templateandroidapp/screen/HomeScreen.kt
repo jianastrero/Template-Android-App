115fd698a858acb671067f18168811bc353a5ee3
Jian James P. Astrero <jianjamesastrero@gmail.com>
2023-07-26 13:10:11 +0800

    Extend HomeScreen with interactivity
    The HomeScreen now includes a TextField for messages and a Slider for changing a value. The data entered can be passed to other components via the onNextClick callback function. The HomeScreen uses a new added ViewModel for managing state. The HomeViewModel is instantiated via the 'by viewModel' delegate and passed to a Column layout. This allows users to interact with the HomeScreen and pass their interactions to the application.

--- app/src/main/java/com/jianastrero/templateandroidapp/screen/HomeScreen.kt
+++ app/src/main/java/com/jianastrero/templateandroidapp/screen/HomeScreen.kt
@@ -1,18 +1,83 @@
 package com.jianastrero.templateandroidapp.screen
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.jianastrero.templateandroidapp.ui.theme.TemplateAndroidAppTheme
+import com.jianastrero.templateandroidapp.viewmodel.domain.IHomeViewModel
+import com.jianastrero.templateandroidapp.viewmodel.implementation.HomeViewModel
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(
-    modifier: Modifier = Modifier
+    onNextClick: (message: String, value: Float) -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: IHomeViewModel = viewModel<HomeViewModel>()
 ) {
-    Column(modifier = modifier) {
-        Text(text = "Home Screen")
+    val state by viewModel.state.collectAsState()
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(64.dp),
+        modifier = Modifier
+            .padding(
+                horizontal = 12.dp,
+                vertical = 64.dp
+            )
+            .then(modifier)
+    ) {
+        Text(
+            text = "Home Screen",
+            color = MaterialTheme.colorScheme.primary,
+            fontWeight = FontWeight.Bold,
+            style = MaterialTheme.typography.headlineLarge,
+        )
+        Spacer(modifier = Modifier.weight(1f))
+        TextField(
+            value = state.message,
+            onValueChange = {
+                viewModel.updateState(state.copy(message = it))
+            },
+            label = { Text("Message") },
+            modifier = Modifier.fillMaxWidth(0.8f)
+        )
+        Slider(
+            value = state.value,
+            onValueChange = {
+                viewModel.updateState(state.copy(value = it))
+            },
+            modifier = Modifier.fillMaxWidth(0.8f)
+        )
+        Spacer(modifier = Modifier.weight(1f))
+        Button(
+            onClick = {
+                onNextClick(state.message, state.value)
+            },
+            modifier = Modifier.fillMaxWidth(0.8f)
+        ) {
+            Text(
+                text = "View Details",
+                fontWeight = FontWeight.Bold,
+                style = MaterialTheme.typography.bodyLarge
+            )
+        }
     }
 }
 
@@ -20,6 +85,9 @@ fun HomeScreen(
 @Composable
 private fun HomeScreenPreview() {
     TemplateAndroidAppTheme {
-        HomeScreen()
+        HomeScreen(
+            onNextClick = { _, _ -> },
+            viewModel = IHomeViewModel.Preview
+        )
     }
 }
