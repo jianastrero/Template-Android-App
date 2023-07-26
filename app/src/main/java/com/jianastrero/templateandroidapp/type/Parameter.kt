aaa112d49ab33d3261fb6911c586ca0bce34cc81
Jian James P. Astrero <jianjamesastrero@gmail.com>
2023-07-26 10:15:10 +0800

    Refactor Argument and Parameter classes to implement NavKeyType
    This commit restructures Argument and Parameter classes under screen package to implement a new interface NavKeyType. The interface implementation replaces direct declaration of 'key' and 'type' properties in Argument and Parameter classes, improving code consistency and readability. This refactoring is part of an effort to streamline navigation argument management, reducing code redundancy and enhancing compatibility with common navigation tools.

--- app/src/main/java/com/jianastrero/templateandroidapp/type/Parameter.kt
+++
@@ -1,7 +0,0 @@
-package com.jianastrero.templateandroidapp.type
-
-import kotlin.reflect.KClass
-
-data class Parameter<T : Any>(val key: String, val type: KClass<T>)
-
-inline fun <reified T : Any> parameter(key: String): Parameter<T> = Parameter(key, T::class)
