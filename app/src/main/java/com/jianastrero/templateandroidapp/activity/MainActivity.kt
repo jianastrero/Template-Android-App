ff2e804a2a39113b725be027767cb370817fb9bb
Jian James P. Astrero <jianjamesastrero@gmail.com>
2023-07-26 18:27:57 +0800

    Implement splash screen in MainActivity.kt
    Added splash screen functionality to improve app initialization user experience. The splash screen runs for a set duration using a CoroutineScope timer. Also implemented ViewTreeObserver.OnPreDrawListener to manage screen displays. Modified MainActivity file to incorporate these changes.

--- app/src/main/java/com/jianastrero/templateandroidapp/activity/MainActivity.kt
+++ app/src/main/java/com/jianastrero/templateandroidapp/activity/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.jianastrero.templateandroidapp.activity
 
 import android.os.Bundle
+import android.view.View
+import android.view.ViewTreeObserver
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
@@ -8,9 +10,18 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.jianastrero.templateandroidapp.navgraph.MainNavGraph
 import com.jianastrero.templateandroidapp.ui.theme.TemplateAndroidAppTheme
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
-class MainActivity : ComponentActivity() {
+class MainActivity : ComponentActivity(), ViewTreeObserver.OnPreDrawListener {
+
+    private var splashScreenTimerStart = 0L
+
+    private lateinit var content: View
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -18,6 +29,29 @@ class MainActivity : ComponentActivity() {
                 MainNavGraph()
             }
         }
+
+        content = findViewById(android.R.id.content)
+        content.viewTreeObserver.addOnPreDrawListener(this)
+
+        CoroutineScope(Dispatchers.Default).launch {
+            splashScreenTimerStart = System.currentTimeMillis()
+            while (System.currentTimeMillis() > splashScreenTimerStart + SPLASH_SCREEN_DURATION) {
+                delay(100)
+            }
+        }
+    }
+
+    override fun onPreDraw(): Boolean {
+        return if (System.currentTimeMillis() > splashScreenTimerStart + SPLASH_SCREEN_DURATION) {
+            content.viewTreeObserver.removeOnPreDrawListener(this)
+            true
+        } else {
+            false
+        }
+    }
+
+    companion object {
+        private const val SPLASH_SCREEN_DURATION = 1_500L
     }
 }
 
