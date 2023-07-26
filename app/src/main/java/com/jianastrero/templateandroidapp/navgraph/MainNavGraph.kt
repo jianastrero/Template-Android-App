5bf2111c1594ba83803f3261bc423832d2d15e3c
Jian James P. Astrero <jianjamesastrero@gmail.com>
2023-07-26 10:16:11 +0800

    Add MainNavGraph.kt to manage navigation
    Introduced new file MainNavGraph.kt in the navgraph package to handle application navigation using Navigation Component. This NavHost can navigate between HomeScreen and DetailScreen via defined routes. An additional function for previewing the UI is also included.

---
+++ app/src/main/java/com/jianastrero/templateandroidapp/navgraph/MainNavGraph.kt
@@ -0,0 +1,41 @@
+package com.jianastrero.templateandroidapp.navgraph
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.jianastrero.templateandroidapp.enumeration.Screen
+import com.jianastrero.templateandroidapp.extension.navigate
+import com.jianastrero.templateandroidapp.screen.DetailScreen
+import com.jianastrero.templateandroidapp.screen.HomeScreen
+
+@Composable
+fun MainNavGraph(navController: NavHostController = rememberNavController()) {
+    NavHost(
+        navController = navController,
+        startDestination = Screen.Home.route
+    ) {
+        composable(Screen.Home.route) {
+            HomeScreen(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .clickable {
+                        navController.navigate(
+                            Screen.Detail,
+                            arguments = mapOf(
+                                Screen.Detail.TITLE to "From Home Screen",
+                                Screen.Detail.VALUE to 0.69f
+                            )
+                        )
+                    }
+            )
+        }
+        composable(Screen.Detail.route, Screen.Detail.namedNavArguments) {
+            DetailScreen(modifier = Modifier.fillMaxSize())
+        }
+    }
+}
