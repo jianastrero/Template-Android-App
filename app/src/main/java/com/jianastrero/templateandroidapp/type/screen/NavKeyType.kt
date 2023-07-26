72ebd777ec12f77a294a6e6053b4dabbbdf6c0a5
Jian James P. Astrero <jianjamesastrero@gmail.com>
2023-07-26 11:20:50 +0800

    Add method to retrieve navigation arguments from bundle
    Added a method in NavKeyType.kt to convert navigation arguments from a bundle object to the appropriate type. This is to enable easier and more consistent handling of navigation arguments throughout the application.

--- app/src/main/java/com/jianastrero/templateandroidapp/type/screen/NavKeyType.kt
+++ app/src/main/java/com/jianastrero/templateandroidapp/type/screen/NavKeyType.kt
@@ -1,5 +1,6 @@
 package com.jianastrero.templateandroidapp.type.screen
 
+import android.os.Bundle
 import androidx.navigation.NavType
 import androidx.navigation.navArgument
 import kotlin.reflect.KClass
@@ -22,4 +23,19 @@ fun NavKeyType<*>.toNavArgument() = navArgument(key) {
         Array<String>::class -> NavType.StringArrayType
         else -> NavType.StringType
     }
+}
+
+fun NavKeyType<*>.fromNavArgument(navArguments: Bundle): Any {
+    return when (type) {
+        Boolean::class -> navArguments.getBoolean(key)
+        Int::class -> navArguments.getInt(key)
+        Long::class -> navArguments.getLong(key)
+        Float::class -> navArguments.getFloat(key)
+        Array<Boolean>::class -> navArguments.getBooleanArray(key)
+        Array<Int>::class -> navArguments.getIntArray(key)
+        Array<Long>::class -> navArguments.getLongArray(key)
+        Array<Float>::class -> navArguments.getFloat(key)
+        Array<String>::class -> navArguments.getStringArray(key)
+        else -> navArguments.getString(key, "")
+    } as Any
 }
