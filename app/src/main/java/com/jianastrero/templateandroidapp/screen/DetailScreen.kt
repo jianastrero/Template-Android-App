14f41d746cfc76c1c0a65e3e5f0dc6d403b44b1e
Jian James P. Astrero <jianjamesastrero@gmail.com>
2023-07-26 16:11:05 +0800

    Revamp DetailsScreen with additional UI components and styling
    Detailed view of the application has been enhanced with the help of `DetailItem` component. UI components for setting the alignment, padding, filling width, color scheme, and font weight were added. The main aim of these changes is to create an organized, easily readable, and visually pleasing layout for detailed information display. Well-defined spaces and bold typography are included for the heading and main area for better separation. Also, valid data was added in `DetailScreenPreview` for previewing the changes effectively.

--- app/src/main/java/com/jianastrero/templateandroidapp/screen/DetailScreen.kt
+++ app/src/main/java/com/jianastrero/templateandroidapp/screen/DetailScreen.kt
@@ -1,10 +1,19 @@
 package com.jianastrero.templateandroidapp.screen
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.jianastrero.templateandroidapp.component.DetailItem
 import com.jianastrero.templateandroidapp.model.detail.DetailArguments
 import com.jianastrero.templateandroidapp.ui.theme.TemplateAndroidAppTheme
 
@@ -13,8 +22,33 @@ fun DetailScreen(
     detailArguments: DetailArguments,
     modifier: Modifier = Modifier
 ) {
-    Column(modifier = modifier) {
-        Text(text = "${detailArguments.title} - ${detailArguments.value}")
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .padding(
+                horizontal = 12.dp,
+                vertical = 64.dp
+            )
+            .then(modifier)
+    ) {
+        Text(
+            text = "Detail Screen",
+            color = MaterialTheme.colorScheme.primary,
+            fontWeight = FontWeight.Bold,
+            style = MaterialTheme.typography.headlineLarge,
+        )
+        Spacer(modifier = Modifier.height(64.dp))
+        DetailItem(
+            title = "Message",
+            value = detailArguments.title,
+            modifier = Modifier.fillMaxWidth()
+        )
+        DetailItem(
+            title = "Value",
+            value = detailArguments.value,
+            modifier = Modifier.fillMaxWidth()
+                .padding(top = 12.dp)
+        )
     }
 }
 
@@ -22,6 +56,6 @@ fun DetailScreen(
 @Composable
 private fun DetailScreenPreview() {
     TemplateAndroidAppTheme {
-        DetailScreen(DetailArguments())
+        DetailScreen(DetailArguments("Hello world!", 0.696969f))
     }
 }
